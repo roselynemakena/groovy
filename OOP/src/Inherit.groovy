897b0ea0120827cd34dd77cff7aadd44d2364dba
Makena <roselynemakena@gmail.com>
2018-10-09 10:52:46 +0300

    Update more files

---
+++ OOP/src/Inherit.groovy
@@ -0,0 +1,23 @@
+class Mammals {
+    def name, noOfLegs, noOfEyes
+
+
+    def feed(){
+
+    }
+    def walk(){
+
+    }
+    def sleep(){
+
+    }
+}
+
+
+class Man extends Mammals{
+    def walk(){
+
+    }
+}
+
+def man = new Man(name: "Adam", noOfEyes: 2, noOfLegs: 2)
